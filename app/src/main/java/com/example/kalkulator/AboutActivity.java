a3863a1ff826f137db7fbe0544546cf763f30420
Mikołaj Rajczyk <mikolajrajczyk01@gmail.com>
2023-03-22 02:48:52 +0100

    wrong activity loaded on about fixed

--- app/src/main/java/com/example/kalkulator/AboutActivity.java
+++ app/src/main/java/com/example/kalkulator/AboutActivity.java
@@ -12,7 +12,7 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_simple);
+        setContentView(R.layout.activity_about);
 
     }
 
